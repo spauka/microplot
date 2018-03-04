76ff481d11e17f88fac3a93c9d925ff85a2b833a
Sebastian Pauka <spauka@gmail.com>
2018-03-04 16:08:49 +1100

    Fix min/max updates

--- series.py
+++ series.py
@@ -29,12 +29,12 @@ class Series():
         if self.ndim == 2:
             self.upd_min_x = min_x is None
             self.upd_max_x = max_x is None
-            self.min_x = min_x if self.upd_min_x else min(self.x)
-            self.max_x = max_x if self.upd_max_x else max(self.x)
+            self.min_x = min_x if not self.upd_min_x else min(self.x)
+            self.max_x = max_x if not self.upd_max_x else max(self.x)
         self.upd_min_y = min_y is None
         self.upd_max_y = max_y is None
-        self.min_y = min_y if self.upd_min_y else min(self.y)
-        self.max_y = max_y if self.upd_max_y else max(self.y)
+        self.min_y = min_y if not self.upd_min_y else min(self.y)
+        self.max_y = max_y if not self.upd_max_y else max(self.y)
 
         # Store marker
         self.marker = marker
@@ -63,16 +63,31 @@ class Series():
             return zip(self.x, self.y)
         return iter(self.y)
 
+    def _upd_x_lim(self, val):
+        if self.upd_min_x and val < self.min_x:
+            self.min_x = val
+        if self.upd_max_x and val > self.max_x:
+            self.max_x = val
+
+    def _upd_y_lim(self, val):
+        if self.upd_min_y and val < self.min_y:
+            self.min_y = val
+        if self.upd_max_y and val > self.max_y:
+            self.max_y = val
+
     def push(self, x_or_val, val=None):
         if self.ndim == 2:
             if val is None:
                 raise ValueError("Both x and val must be given for 2-dimensional array")
             self.x.push(x_or_val)
             self.y.push(val)
+            self._upd_x_lim(x_or_val)
+            self._upd_y_lim(val)
         else:
             if val is not None:
                 raise ValueError("Only 1 value should be given for for a 1-dimensional array")
             self.y.push(x_or_val)
+            self._upd_y_lim(x_or_val)
 
 class BoundedSeries(Series):
     def __init__(self, max_len, x_or_y, y=None, *args, **kwargs):
@@ -91,9 +106,11 @@ class BoundedSeries(Series):
             self.x = [0]*max_len
             for i, pt in enumerate(x[-max_len:]):
                 self.x[i] = pt
+            self._upd_x_lim(None)
         self.y = [0]*max_len
         for i, pt in enumerate(y[-max_len:]):
             self.y[i] = pt
+        self._upd_y_lim(None)
 
         # And keep track of the filled length, and the next index to fill
         self.len = min(self.max_len, len(self.y))
@@ -130,14 +147,29 @@ class BoundedSeries(Series):
                 ind = (ind + 1) % length
         return circ_iter(self.next_slot, self.max_len)
 
+    def _upd_x_lim(self, val):
+        if self.upd_min_x:
+            self.min_x = min(self.x)
+        if self.upd_max_x:
+            self.max_x = max(self.x)
+
+    def _upd_y_lim(self, val):
+        if self.upd_min_y:
+            self.min_y = min(self.y)
+        if self.upd_max_y:
+            self.max_y = max(self.y)
+
     def push(self, x_or_val, val=None):
         if self.ndim == 2:
             if val is None:
                 raise ValueError("Both x and y must be given for a 2-dimensional array")
             self.x[self.next_slot] = x_or_val
             self.y[self.next_slot] = val
+            self._upd_x_lim(x_or_val)
+            self._upd_y_lim(val)
         else:
             if val is not None:
                 raise ValueError("Only one parameter may be given for a 1-dimensional array")
             self.y[self.next_slot] = x_or_val
+            self._upd_y_lim(x_or_val)
         self.next_slot = (self.next_slot + 1) % self.max_len
